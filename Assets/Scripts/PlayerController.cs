c22cfa2022584568227f62033096f246cd4b6a86
unknown <adamschoenemann@gmail.com>
2013-11-06 15:26:33 +0100

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("PlayerController staring...");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
